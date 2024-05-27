6fa47dd721d2092c697dced183817358b9e93d55
QuantumTrolling <st097173@student.spbu.ru>
2024-05-27 20:59:19 +0300

    few fixies

--- src/main/java/net/creatorsyndrome/storyengine/init/StoryengineModMenus.java
+++ src/main/java/net/creatorsyndrome/storyengine/init/StoryengineModMenus.java
@@ -7,7 +7,6 @@ package net.creatorsyndrome.storyengine.init;
 import net.minecraftforge.network.IContainerFactory;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.RegistryEvent;
 
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.inventory.AbstractContainerMenu;
